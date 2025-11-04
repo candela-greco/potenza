66f8f3854f0b0304a178cf98695a4c67da7c9123
candela-greco <candelagreco15@gmail.com>
2025-11-04 15:57:47 -0300

    fix: rewrite and vite base config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // <- esto asegura que las rutas sean relativas a la raíz
+  base: './', // <- Cambia esto a './' en lugar de '/'
   build: {
     outDir: 'dist',
-  },
+    assetsDir: 'assets'
+  }
 })
